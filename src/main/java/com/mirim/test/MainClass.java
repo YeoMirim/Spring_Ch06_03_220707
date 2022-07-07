5bed615ca962b990bfb6f75446e0176eced31d2f
ATE <ATE@DESKTOP-ODIPDNU>
2022-07-07 16:33:33 +0900

    Spring_Bean_XML+Java_220707

---
+++ src/main/java/com/mirim/test/MainClass.java
@@ -0,0 +1,25 @@
+package com.mirim.test;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationCTX.xml");
+		Student stu1 = ctx.getBean("student1", Student.class);
+		Student stu2 = ctx.getBean("student2", Student.class);
+		Student stu3 = ctx.getBean("student3", Student.class);
+		
+		System.out.println(stu1.getName());
+		System.out.println(stu2.getName());
+		System.out.println(stu3.getName());
+		
+		ctx.close();
+	}
+
+}
+
+// java파일과 xml파일에서 다 읽어옴
